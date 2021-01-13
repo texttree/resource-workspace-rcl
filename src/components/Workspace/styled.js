e4b04653d62725d955ce036170c4c0e871ae2033
Manny Colon <colonmanuel7@gmail.com>
2021-01-12 21:18:15 -0500

    Update styled.js

--- src/components/Workspace/styled.js
+++ src/components/Workspace/styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   height: 100%;
-  background-color: white;
+  background-color: 'transparent';
   .react-grid-item.react-grid-placeholder {
     background-color: ${props => `${props.dragBackgroundColor} !important`};
   }  
